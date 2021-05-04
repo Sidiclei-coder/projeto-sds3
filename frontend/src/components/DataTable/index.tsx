cb3a673dcd6d7f898d40882321ee5643b77290f7
Sidiclei <sidicleiluiz@hotmail.com>
2021-05-04 19:03:50 -0300

    Static charts

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -14,7 +14,7 @@ const DataTable = () => {
                 <tbody>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
+                        <td>Anakin</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
